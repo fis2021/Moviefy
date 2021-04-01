27e57b03663b91f2061a26b6acf791a50cd9c056
Ioan Hedeș <ihedes13@gmail.com>
2021-04-01 15:28:19 +0300

    MOV-53 Added basic Gradle project structure.

---
+++ src/main/java/org/loose/fis/mov/Main.java
@@ -0,0 +1,17 @@
+package org.loose.fis.mov;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
