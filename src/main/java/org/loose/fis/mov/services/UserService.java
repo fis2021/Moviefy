b65b4a7801e61a31bb7684dcc79728369e4d0827
Ioan Hedeș <ihedes13@gmail.com>
2021-04-26 20:35:07 +0300

    MOV-3 Added password encryption for database storing.

--- src/main/java/org/loose/fis/mov/services/UserService.java
+++ src/main/java/org/loose/fis/mov/services/UserService.java
@@ -3,8 +3,10 @@ package org.loose.fis.mov.services;
 import org.loose.fis.mov.exceptions.*;
 import org.loose.fis.mov.model.User;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Objects;
-import java.util.regex.Pattern;
 
 public class UserService {
     public static void addUser(String username, String firstname, String lastname, String password, String email, String role,
@@ -14,7 +16,8 @@ public class UserService {
         if (Objects.equals(role, "Admin")) {
             CinemaService.addCinema(cinemaName, username, cinemaAddress, Integer.parseInt(cinemaCapacity));
         }
-        DatabaseService.getUserRepo().insert(new User(username, firstname, lastname, password, email, role));
+        DatabaseService.getUserRepo().insert(new User(username, firstname, lastname,
+                UserService.encodePassword(username, password), email, role));
     }
 
     private static void checkUserAlreadyExists(String username) throws UserAlreadyExistsException {
@@ -31,5 +34,25 @@ public class UserService {
                 throw new EmailAddressAlreadyUsedException(email);
             }
         }
+    }
+
+    private static String encodePassword(String salt, String password) {
+        MessageDigest md = getMessageDigest();
+        md.update(salt.getBytes(StandardCharsets.UTF_8));
+
+        byte[] hashedPassword = md.digest(password.getBytes(StandardCharsets.UTF_8));
+
+        return new String(hashedPassword, StandardCharsets.UTF_8)
+                .replace("\"", ""); //to be able to save in JSON format
+    }
+
+    private static MessageDigest getMessageDigest() {
+        MessageDigest md;
+        try {
+            md = MessageDigest.getInstance("SHA-512");
+        } catch (NoSuchAlgorithmException e) {
+            throw new IllegalStateException("SHA-512 does not exist!");
+        }
+        return md;
     }
 }
