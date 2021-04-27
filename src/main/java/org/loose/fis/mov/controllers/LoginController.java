e5d414237d9892e5e5ae816eafb69f53060a58e2
Ioan Hedeș <ihedes13@gmail.com>
2021-04-27 18:56:03 +0300

    MOV-4 Designed Login page

---
+++ src/main/java/org/loose/fis/mov/controllers/LoginController.java
@@ -0,0 +1,23 @@
+package org.loose.fis.mov.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+    @FXML
+    private TextField usernameField;
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    public void handleLoginAction() {
+
+    }
+
+    @FXML
+    public void handleChangePasswordAction() {
+
+    }
+}
