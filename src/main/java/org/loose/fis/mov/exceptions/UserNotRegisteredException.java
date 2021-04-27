d85003162718c77f538479ecfa7f76147046a519
Ioan Hedeș <ihedes13@gmail.com>
2021-04-27 21:09:42 +0300

    MOV-4 Made the UserNotRegisteredException a little more general.

--- src/main/java/org/loose/fis/mov/exceptions/UserNotRegisteredException.java
+++ src/main/java/org/loose/fis/mov/exceptions/UserNotRegisteredException.java
@@ -1,10 +1,7 @@
 package org.loose.fis.mov.exceptions;
 
 public class UserNotRegisteredException extends Exception {
-    private String username;
-
-    public UserNotRegisteredException(String username) {
-        super(String.format("A user with the name %s does not exist!", username));
-        this.username = username;
+    public UserNotRegisteredException() {
+        super("This user does not exist!");
     }
 }
