e7299ef7a971cb41829e6a4b4604b63ca5d022b1
Ioan Hedeș <ihedes13@gmail.com>
2021-04-27 17:21:32 +0300

    MOV-3 Modified the servicing classes for easier use when unit testing.

--- src/main/java/org/loose/fis/mov/services/DatabaseService.java
+++ src/main/java/org/loose/fis/mov/services/DatabaseService.java
@@ -13,18 +13,22 @@ public class DatabaseService {
     private static ObjectRepository<Screening> screeningRepo;
     private static ObjectRepository<Booking> bookingRepo;
     private static ObjectRepository<Review> reviewRepo;
+    private static Nitrite database;
 
     public static void initDatabase() {
-        Nitrite database = Nitrite.builder()
-                .filePath(getPathToFile("moviefy_test.db").toFile())
+        database = Nitrite.builder()
+                .filePath(FileSystemService.getPathToFile("moviefy_test.db").toFile())
                 .openOrCreate();
-
         userRepo = database.getRepository(User.class);
         cinemaRepo = database.getRepository(Cinema.class);
         movieRepo = database.getRepository(Movie.class);
         screeningRepo = database.getRepository(Screening.class);
         bookingRepo = database.getRepository(Booking.class);
         reviewRepo = database.getRepository(Review.class);
+    }
+
+    public static void closeDatabase() {
+        database.close();
     }
 
     public static ObjectRepository<User> getUserRepo() {
