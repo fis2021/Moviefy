9bd249f3b6b65b6390872457c47e1088fa7ea1b9
Ioan Hedeș <ihedes13@gmail.com>
2021-04-29 17:32:23 +0300

    MOV-37 Linked the top buttons to their respective onAction methods.
    The Menu type box needed to be replaced, as it didn't support onAction calls. Until a better UI is designed, these buttons will be replaced by hyperlinks.
    
    The logout and home buttons work. The other two are not yet implemented.

--- src/main/java/org/loose/fis/mov/controllers/MainMenuAdminController.java
+++ src/main/java/org/loose/fis/mov/controllers/MainMenuAdminController.java
@@ -1,10 +1,15 @@
 package org.loose.fis.mov.controllers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
+import org.loose.fis.mov.exceptions.SessionDoesNotExistException;
 import org.loose.fis.mov.model.ScreeningListCell;
 import org.loose.fis.mov.model.Screening;
+import org.loose.fis.mov.services.CommService;
+import org.loose.fis.mov.services.UserService;
 
+import java.io.IOException;
 import java.util.Date;
 
 public class MainMenuAdminController extends AbstractController {
@@ -25,6 +30,26 @@ public class MainMenuAdminController extends AbstractController {
             list.getItems().add(new Screening(null, new Date(System.currentTimeMillis()), "puerto niko", "cinema dacia", 12));
         }
         list.setPrefHeight(ITEM_NO * list.getFixedCellSize() + 2);
+    }
 
+    @FXML
+    public void handleLogout(ActionEvent event) throws SessionDoesNotExistException, IOException {
+        System.out.println("merge?");
+        UserService.logout();
+        changeScene(event, "login.fxml");
+    }
+
+    @FXML
+    public void handleHome(ActionEvent event) throws IOException {
+        changeScene(event, "mainMenuAdmin.fxml");
+    }
+
+    @FXML
+    public void handleMyProfile(ActionEvent actionEvent) {
+        System.out.println("This would have taken you to your profile. It's a shame that page doesn't even exist yet.");
+    }
+
+    public void handleAddScreening(ActionEvent actionEvent) {
+        System.out.println("This would have taken you to the Add Screening page. We do not have such capabilities yet.");
     }
 }
