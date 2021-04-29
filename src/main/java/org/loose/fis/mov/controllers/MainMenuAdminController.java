1c7f74aa48ec9c8137199c89cb136f35d546d6e4
Ioan Hedeș <ihedes13@gmail.com>
2021-04-29 14:25:20 +0300

    MOV-37 Working on a resizeable scrolling list in the Admin Landing Page.
    Created a scrollable list that resizes based on the number of items it has inside. Currently everything inside it is hardcoded, since we have no movies in the database.
    Need to work on making the list cells custom before moving on to the top buttons.

--- src/main/java/org/loose/fis/mov/controllers/MainMenuAdminController.java
+++ src/main/java/org/loose/fis/mov/controllers/MainMenuAdminController.java
@@ -1,6 +1,37 @@
 package org.loose.fis.mov.controllers;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import org.loose.fis.mov.model.Movie;
 
 public class MainMenuAdminController extends AbstractController {
+
+    private VBox[] movies = new VBox[10];
+
+    private Text[] texts = new Text[10];
+
+    @FXML
+    private ScrollPane scroll;
+
+    @FXML
+    private ListView list;
+
+    @FXML
+    public void initialize() {
+        int ROW_SIZE = 24;
+        int ITEM_NO = 5;
+        try {
+            for (int i = 0; i < ITEM_NO; i++) {
+                System.out.println("merge");
+                list.getItems().add(new Movie("salut", "fff", 23).getTitle());
+            }
+            list.setPrefHeight(ITEM_NO * ROW_SIZE);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
