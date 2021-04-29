a9a36987513164baf8301c78bee3ce24ad2b12fe
Ioan Hedeș <ihedes13@gmail.com>
2021-04-29 19:26:00 +0300

    MOV-37 The Admin landing page now displays the future screenings.
    After logging in, an Admin will be prompted with the list of oncoming screenings for their cinema.
    
    This wasn't thoroughly tested. The manual tests I did worked, trying some unit tests tomorrow.

--- src/main/java/org/loose/fis/mov/controllers/MainMenuAdminController.java
+++ src/main/java/org/loose/fis/mov/controllers/MainMenuAdminController.java
@@ -1,40 +1,58 @@
 package org.loose.fis.mov.controllers;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
+import javafx.scene.text.Text;
 import org.loose.fis.mov.exceptions.SessionDoesNotExistException;
+import org.loose.fis.mov.exceptions.UserNotAdminException;
+import org.loose.fis.mov.exceptions.UserNotRegisteredException;
 import org.loose.fis.mov.model.ScreeningListCell;
 import org.loose.fis.mov.model.Screening;
-import org.loose.fis.mov.services.CommService;
-import org.loose.fis.mov.services.UserService;
+import org.loose.fis.mov.services.*;
 
 import java.io.IOException;
+import java.util.Calendar;
 import java.util.Date;
+
+
+import static org.dizitart.no2.objects.filters.ObjectFilters.eq;
+import static org.dizitart.no2.objects.filters.ObjectFilters.gt;
 
 public class MainMenuAdminController extends AbstractController {
     private static final int CELL_SIZE = 30;
+    private ObservableList<Screening> observableList;
 
+    @FXML
+    private Text pageTitle;
     @FXML
     private ListView<Screening> list;
 
     @FXML
     public void initialize() {
-        /* these values w */
-        list.setFixedCellSize(CELL_SIZE);
-        int ITEM_NO = 5;
+        try {
+            observableList = FXCollections.observableList(
+                    ScreeningService.findAllFutureScreeningsForCinema(
+                            CinemaService.findCinemaForAdmin(
+                                    SessionService.getLoggedInUser()
+                            )
+                    )
+            );
+            list.setFixedCellSize(CELL_SIZE);
 
-        /* changing the ListView to use our custom List Cells instead of the default ones */
-        list.setCellFactory(param -> new ScreeningListCell());
-        for (int i = 0; i < ITEM_NO; i++) {
-            list.getItems().add(new Screening(null, new Date(System.currentTimeMillis()), "puerto niko", "cinema dacia", 12));
+            /* changing the ListView to use our custom List Cells instead of the default ones */
+            list.setCellFactory(param -> new ScreeningListCell());
+            list.setItems(observableList);
+            list.setPrefHeight(observableList.size() * list.getFixedCellSize() + 2);
+        } catch (UserNotAdminException e) {
+            pageTitle.setText(e.getMessage());
         }
-        list.setPrefHeight(ITEM_NO * list.getFixedCellSize() + 2);
     }
 
     @FXML
     public void handleLogout(ActionEvent event) throws SessionDoesNotExistException, IOException {
-        System.out.println("merge?");
         UserService.logout();
         changeScene(event, "login.fxml");
     }
