16f27e850b02a04645ddf4da24d5061e99d3a1ed
Ioan Hedeș <ihedes13@gmail.com>
2021-04-30 15:13:56 +0300

    MOV-37 Added Unit tests for the static methods used in the Admin Main.
    The methods created for displaying the Admin's cinema's future screenings were unit tested.

--- src/test/java/org/loose/fis/mov/services/CinemaServiceTest.java
+++ src/test/java/org/loose/fis/mov/services/CinemaServiceTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.loose.fis.mov.exceptions.CinemaAlreadyExistsException;
+import org.loose.fis.mov.exceptions.UserNotAdminException;
+import org.loose.fis.mov.model.User;
 
 import java.io.IOException;
 
@@ -30,5 +32,49 @@ class CinemaServiceTest {
     void addCinema() {
         assertDoesNotThrow(() -> CinemaService.addCinema("test", "test", "test", 10));
         assertThrows(CinemaAlreadyExistsException.class, () -> CinemaService.addCinema("test", "test", "test", 10));
+    }
+
+    @Test
+    void findCinemaForAdminStringSuccess() {
+        try {
+            UserService.addUser("test", "test", "test", "test_test", "test@test.test",
+                    "Admin", "test", "test", "10");
+            assertEquals("test", CinemaService.findCinemaForAdmin("test").getAdminUsername());
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+    }
+
+    @Test
+    void findCinemaForAdminStringFail() {
+        try {
+            UserService.addUser("test", "test", "test", "test_test", "test@test.test",
+                    "Client", "", "", "");
+            assertThrows(UserNotAdminException.class, () -> CinemaService.findCinemaForAdmin("test"));
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+    }
+
+    @Test
+    void findCinemaForAdminUserSuccess() {
+        try {
+            User user = UserService.addUser("test", "test", "test", "test_test", "test@test.test",
+                    "Admin", "test", "test", "10");
+            assertEquals("test", CinemaService.findCinemaForAdmin(user).getAdminUsername());
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+    }
+
+    @Test
+    void findCinemaForAdminUserFail() {
+        try {
+            User user = UserService.addUser("test", "test", "test", "test_test", "test@test.test",
+                    "Client", "", "", "");
+            assertThrows(UserNotAdminException.class, () -> CinemaService.findCinemaForAdmin(user));
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
     }
 }
