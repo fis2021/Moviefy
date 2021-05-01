d531b3ae07ebc8e32ce050341b09ebde311cb09a
Ioan Hedeș <ihedes13@gmail.com>
2021-05-01 15:31:45 +0300

    MOV-37 Some temporary lines added for debugging this task.

--- src/main/java/org/loose/fis/mov/Main.java
+++ src/main/java/org/loose/fis/mov/Main.java
@@ -5,8 +5,12 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.loose.fis.mov.model.Screening;
 import org.loose.fis.mov.services.DatabaseService;
 import org.loose.fis.mov.services.FileSystemService;
+
+import java.util.Calendar;
+import java.util.Date;
 
 public class Main extends Application {
 
@@ -14,6 +18,16 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
         FileSystemService.initDirectory();
         DatabaseService.initDatabase();
+
+        /* TEMPORARY FOR THE DEVELOPMENT OF MOV-37 */
+//        DatabaseService.getScreeningRepo().insert(new Screening(null, new Date(121, Calendar.APRIL, 29, 21, 30), "Iohannis in actiune", "Cinema Timis", 13));
+//        DatabaseService.getScreeningRepo().insert(new Screening(null, new Date(121, Calendar.APRIL, 29, 19, 00), "Cum sa nu te pierzi pana in centru", "Cinema Timis", 21));
+//        //DatabaseService.getScreeningRepo().insert(new Screening(null, new Date(2021, 4, 28, 16, 35), "Dragonu scoate album", "Cinema Timis", 0));
+//        DatabaseService.getScreeningRepo().insert(new Screening(null, new Date(121, Calendar.APRIL, 30, 11, 35), "Dragonu scoate album", "Cinema Braila", 0));
+//        DatabaseService.getScreeningRepo().insert(new Screening(null, new Date(121, Calendar.MAY, 31, 11, 35), "Film amusant", "Cinema Timis", 1));
+
+        /* TEMPORARY FOR THE DEVELOPMENT OF MOV-37 */
+
         Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("login.fxml"));
         primaryStage.setTitle("Moviefy");
         primaryStage.setResizable(false);
