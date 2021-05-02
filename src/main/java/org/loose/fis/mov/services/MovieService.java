e5da14f517817e721f458aa83a8dcb1005295e47
Ioan Hedeș <ihedes13@gmail.com>
2021-05-02 19:40:20 +0300

    MOV-9 Added static method for convenience -> check description.
    Static method for providing all Movies NOT screened at a cinema.

--- src/main/java/org/loose/fis/mov/services/MovieService.java
+++ src/main/java/org/loose/fis/mov/services/MovieService.java
@@ -2,7 +2,33 @@ package org.loose.fis.mov.services;
 
 import org.dizitart.no2.Nitrite;
 import org.dizitart.no2.objects.ObjectRepository;
+import org.loose.fis.mov.model.Cinema;
 import org.loose.fis.mov.model.Movie;
+import org.loose.fis.mov.model.Screening;
+
+import javax.xml.crypto.Data;
+import java.util.List;
+
+import static org.dizitart.no2.objects.filters.ObjectFilters.and;
+import static org.dizitart.no2.objects.filters.ObjectFilters.eq;
 
 public class MovieService {
+    public static List<Movie> getMoviesNotScreenedAtCinema(Cinema cinema) {
+        ObjectRepository<Movie> movieRepo = DatabaseService.getMovieRepo();
+        List<Movie> retVal = movieRepo.find().toList();
+        ObjectRepository<Screening> screeningRepo = DatabaseService.getScreeningRepo();
+
+        for (Movie movie : movieRepo.find()) {
+            if (screeningRepo.find(
+                    and (
+                            eq("cinemaName", cinema.getName()),
+                            eq("movieName", movie.getTitle())
+                    )
+            ).firstOrDefault() != null) {
+                retVal.remove(movie);
+            }
+        }
+
+        return retVal;
+    }
 }
