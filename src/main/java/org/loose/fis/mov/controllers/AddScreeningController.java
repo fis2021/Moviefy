0982278ef72cd83e30d21191beff72083a541530
Ioan Hedeș <ihedes13@gmail.com>
2021-05-02 18:53:16 +0300

    MOV-6 Added options to the dropdowns related to screening date.

--- src/main/java/org/loose/fis/mov/controllers/AddScreeningController.java
+++ src/main/java/org/loose/fis/mov/controllers/AddScreeningController.java
@@ -1,5 +1,7 @@
 package org.loose.fis.mov.controllers;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
@@ -9,6 +11,11 @@ import org.loose.fis.mov.exceptions.SessionDoesNotExistException;
 import org.loose.fis.mov.services.UserService;
 
 import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class AddScreeningController extends AbstractController{
     @FXML
@@ -18,15 +25,31 @@ public class AddScreeningController extends AbstractController{
     @FXML
     public TextArea movieDescriptionField;
     @FXML
-    public ComboBox screeningDay;
+    public ComboBox<Integer> screeningDay;
     @FXML
-    public ComboBox screeningMonth;
+    public ComboBox<Integer> screeningMonth;
     @FXML
-    public ComboBox screeningYear;
+    public ComboBox<Integer> screeningYear;
     @FXML
-    public ComboBox screeningHour;
+    public ComboBox<Integer> screeningHour;
     @FXML
-    public ComboBox screeningMinute;
+    public ComboBox<Integer> screeningMinute;
+
+    @FXML
+    public void initialize() {
+        ObservableList<Integer> dayList = FXCollections.observableList(IntStream.rangeClosed(1, 31).boxed().collect(Collectors.toList()));
+        screeningDay.setItems(dayList);
+        ObservableList<Integer> monthList = FXCollections.observableList(IntStream.rangeClosed(1, 12).boxed().collect(Collectors.toList()));
+        screeningMonth.setItems(monthList);
+        ObservableList<Integer> yearList = FXCollections.observableList(IntStream.rangeClosed(2021, 2031).boxed().collect(Collectors.toList()));
+        screeningYear.setItems(yearList);
+        ObservableList<Integer> hourList = FXCollections.observableList(IntStream.rangeClosed(0, 23).boxed().collect(Collectors.toList()));
+        screeningHour.setItems(hourList);
+        ObservableList<Integer> minuteList = FXCollections.observableList(IntStream.rangeClosed(0, 59).boxed().collect(Collectors.toList()));
+        screeningMinute.setItems(minuteList);
+
+        
+    }
 
     @FXML
     public void handleAddScreening(ActionEvent event) {
