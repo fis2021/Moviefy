5a34be5bd66eaf95a85bfe5752af142466bb3a4a
Ioan Hedeș <ihedes13@gmail.com>
2021-05-03 14:39:10 +0300

    MOV-6 Added static method for checking if time intervals are overlapping
    This will be useful when checking the availability of time intervals for adding screenings.
    This method is unit tested.

--- src/main/java/org/loose/fis/mov/services/CommService.java
+++ src/main/java/org/loose/fis/mov/services/CommService.java
@@ -7,6 +7,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.PasswordField;
 import javafx.stage.Stage;
+import javafx.util.Pair;
 import org.loose.fis.mov.exceptions.EmailFormatInvalidException;
 import org.loose.fis.mov.exceptions.EmptyFieldException;
 import org.loose.fis.mov.exceptions.PasswordTooWeakException;
@@ -21,6 +22,7 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.Date;
 import java.util.Objects;
 import java.util.Properties;
 import java.util.regex.Pattern;
@@ -41,6 +43,7 @@ public class CommService {
             }
         }
     }
+
     private static String encodePassword(String salt, String password) {
         MessageDigest md = getMessageDigest();
         md.update(salt.getBytes(StandardCharsets.UTF_8));
@@ -50,6 +53,7 @@ public class CommService {
         return new String(hashedPassword, StandardCharsets.UTF_8)
                 .replace("\"", ""); //to be able to save in JSON format
     }
+
     private static MessageDigest getMessageDigest() {
         MessageDigest md;
         try {
@@ -59,55 +63,76 @@ public class CommService {
         }
         return md;
     }
+
     private void checkMinimumPasswordStrength(PasswordField password) throws PasswordTooWeakException {
         if (password.getText().length() < 8) {
             throw new PasswordTooWeakException();
         }
     }
 
-    public static String WordGenerator(int m){
+    public static String WordGenerator(int m) {
         StringBuilder sb = new StringBuilder();
 
-        String set ="1234567890-=][poiuytrewqasdfghjkl;'/.,mnbvcxzZXCVBNM,./';LKJHGFDSAQWERTYUIOP[]=-!@#$%^&*()zsefbhtsdo";
+        String set = "1234567890-=][poiuytrewqasdfghjkl;'/.,mnbvcxzZXCVBNM,./';LKJHGFDSAQWERTYUIOP[]=-!@#$%^&*()zsefbhtsdo";
 
-        for (int i= 0; i < m; i++) {
-            int k = (int) (100*Math.random());
+        for (int i = 0; i < m; i++) {
+            int k = (int) (100 * Math.random());
             sb.append(set.charAt(k));
         }
         String result = sb.toString();
         return result;
     }
-public static void sendMail(String emailRecipient,String setSubject,String setText){
-    String host = "smtp.gmail.com";
-    Properties properties = System.getProperties();
-    properties.put("mail.smtp.host", host);
-    properties.put("mail.smtp.port", "465");
 
-    properties.put("mail.smtp.ssl.enable", "true");
-    properties.put("mail.smtp.auth", "true");
-    Session session = Session.getInstance(properties, new javax.mail.Authenticator() {
-        protected PasswordAuthentication getPasswordAuthentication() {
-            return new PasswordAuthentication("geani.gibilan", "mojojojo13");
+    public static void sendMail(String emailRecipient, String setSubject, String setText) {
+        String host = "smtp.gmail.com";
+        Properties properties = System.getProperties();
+        properties.put("mail.smtp.host", host);
+        properties.put("mail.smtp.port", "465");
+
+        properties.put("mail.smtp.ssl.enable", "true");
+        properties.put("mail.smtp.auth", "true");
+        Session session = Session.getInstance(properties, new javax.mail.Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication("geani.gibilan", "mojojojo13");
+            }
+        });
+        try {
+            // Create a default MimeMessage object.
+            MimeMessage message = new MimeMessage(session);
+            // Set From: header field of the header.
+            message.setFrom(new InternetAddress("geani.gibilan@gmail.com"));
+            // Set To: header field of the header.
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(emailRecipient));
+            // Set Subject: header field
+            message.setSubject(setSubject);
+            // Now set the actual message
+            message.setText(setText);
+            // System.out.println("sending...");
+            // Send message
+            Transport.send(message);
+            //System.out.println("Sent message successfully....");
+        } catch (MessagingException mex) {
+            mex.printStackTrace();
         }
-    });
-    try {
-        // Create a default MimeMessage object.
-        MimeMessage message = new MimeMessage(session);
-        // Set From: header field of the header.
-        message.setFrom(new InternetAddress("geani.gibilan@gmail.com"));
-        // Set To: header field of the header.
-        message.addRecipient(Message.RecipientType.TO, new InternetAddress(emailRecipient));
-        // Set Subject: header field
-        message.setSubject(setSubject);
-        // Now set the actual message
-        message.setText(setText);
-       // System.out.println("sending...");
-        // Send message
-        Transport.send(message);
-        //System.out.println("Sent message successfully....");
-    } catch (MessagingException mex) {
-        mex.printStackTrace();
     }
-}
+
+    /* the A interval is where I want to insert a new screening / the B interval is an already existing screening */
+    /* the key is the lower margin of the interval / the value is the upper margin of the interval */
+    public static boolean areIntervalsOverlapping(Pair<Date, Date> intervalA, Pair<Date, Date> intervalB) {
+        // case 1: interval B contains the beginning of interval A || interval B contains interval A;
+        if (intervalA.getKey().compareTo(intervalB.getKey()) >= 0 && intervalA.getKey().compareTo(intervalB.getValue()) <= 0) {
+            return true;
+        }
+        // case 2: interval B contains the end of interval A || interval B contains interval A;
+        else if (intervalA.getValue().compareTo(intervalB.getKey()) >= 0 && intervalA.getValue().compareTo(intervalB.getValue()) <= 0) {
+            return true;
+        }
+        // case 3: interval A contains interval B;
+        else if (intervalA.getKey().compareTo(intervalB.getKey()) < 0 && intervalA.getValue().compareTo(intervalB.getValue()) > 0) {
+            return true;
+        }
+        // case 4: the intervals are not overlapping;
+        return false;
+    }
 
 }
---
+++ src/test/java/org/loose/fis/mov/services/CommServiceTest.java
@@ -0,0 +1,62 @@
+package org.loose.fis.mov.services;
+
+import javafx.util.Pair;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CommServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void areIntervalsOverlappingTest() {
+        Pair<Date, Date> intervalA = new Pair<>(
+                new GregorianCalendar(2021, Calendar.MARCH, 25, 10, 30).getTime(),
+                new GregorianCalendar(2021, Calendar.MARCH, 25, 14, 45).getTime()
+        );
+        Pair<Date, Date> intervalB = new Pair<>(
+                new GregorianCalendar(2020, Calendar.MARCH, 25, 9, 21).getTime(),
+                new GregorianCalendar(2020, Calendar.MARCH, 25, 10, 29).getTime()
+        );
+        Pair<Date, Date> intervalC = new Pair<>(
+                new GregorianCalendar(2021, Calendar.MARCH, 25, 9, 59).getTime(),
+                new GregorianCalendar(2021, Calendar.MARCH, 25, 17, 24).getTime()
+        );
+        Pair<Date, Date> intervalD = new Pair<>(
+                new GregorianCalendar(2021, Calendar.MARCH, 25, 12, 31).getTime(),
+                new GregorianCalendar(2021, Calendar.MARCH, 25, 14, 46).getTime()
+        );
+        Pair<Date, Date> intervalE = new Pair<>(
+                new GregorianCalendar(2021, Calendar.MARCH, 25, 10, 30).getTime(),
+                new GregorianCalendar(2021, Calendar.MARCH, 25, 14, 45).getTime()
+        );
+        Pair<Date, Date> intervalF = new Pair<>(
+                new GregorianCalendar(2021, Calendar.MARCH, 25, 9, 30).getTime(),
+                new GregorianCalendar(2021, Calendar.MARCH, 25, 11, 45).getTime()
+        );
+        Pair<Date, Date> intervalG = new Pair<>(
+                new GregorianCalendar(2021, Calendar.MARCH, 25, 11, 30).getTime(),
+                new GregorianCalendar(2021, Calendar.MARCH, 25, 13, 45).getTime()
+        );
+
+        assertFalse(CommService.areIntervalsOverlapping(intervalA, intervalB)); // not overlapping;
+        assertTrue(CommService.areIntervalsOverlapping(intervalA, intervalC)); // C contains A;
+        assertTrue(CommService.areIntervalsOverlapping(intervalA, intervalD)); // D contains end of A;
+        assertTrue(CommService.areIntervalsOverlapping(intervalA, intervalE)); // E equals A;
+        assertTrue(CommService.areIntervalsOverlapping(intervalA, intervalF)); // F contains start of A;
+        assertTrue(CommService.areIntervalsOverlapping(intervalA, intervalG)); // A contains G;
+    }
+}
