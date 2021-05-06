ced3d281401aa7f0c453ad48c20c04f321b142c1
Ioan Hedeș <ihedes13@gmail.com>
2021-05-06 15:06:47 +0300

    MOV-11 Changed the ListView to a TableView.
    As we do not need custom buttons for each entry in this list, a TableView felt better than a ListView with custom cells.
    The collumns are not yet populated with the correct data.

--- src/main/java/org/loose/fis/mov/controllers/BookingListController.java
+++ src/main/java/org/loose/fis/mov/controllers/BookingListController.java
@@ -1,24 +1,64 @@
 package org.loose.fis.mov.controllers;
 
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.Priority;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import org.loose.fis.mov.model.Booking;
-import org.loose.fis.mov.services.UserService;
+import org.loose.fis.mov.model.Screening;
+import org.loose.fis.mov.model.User;
+import org.loose.fis.mov.services.*;
 
 import java.io.IOException;
+import java.util.List;
 
 public class BookingListController extends AbstractController {
 
     @FXML
     private Text pageTitle;
     @FXML
-    private ListView<Booking> list;
+    public TableColumn<Booking, String> firstNameColumn;
+    @FXML
+    public TableColumn<Booking, String> lastNameColumn;
+    @FXML
+    public TableColumn<Booking, String> emailNameColumn;
+    @FXML
+    public TableColumn<Booking, Integer> seatNumberColumn;
+    @FXML
+    private TableView<Booking> table;
 
     @FXML
     public void initialize() {
-        pageTitle.setText("Bookings for movie");
+        Screening selectedScreening = SessionService.getSelectedScreening();
+        pageTitle.setText(
+                "Bookings for movie "
+                        + selectedScreening.getMovieTitle()
+                        + " on "
+                        + CommService.extractDate(selectedScreening.getDate())
+                        + " "
+                        + CommService.extractTime(selectedScreening.getDate())
+        );
+        ObservableList<Booking> observableList =
+                FXCollections.observableList(BookingService.findBookingsAtScreening(selectedScreening));
+
+        /* setting the appropiate info for each column - not yet */
+
+        firstNameColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getClientName()));
+        lastNameColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getClientName()));
+        emailNameColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getClientName()));
+        seatNumberColumn.setCellValueFactory(cellData -> new SimpleObjectProperty<Integer>(cellData.getValue().getNumberOfSeats()));
+        table.setItems(observableList);
     }
 
     @FXML
@@ -42,3 +82,4 @@ public class BookingListController extends AbstractController {
         changeScene(event, "login.fxml");
     }
 }
+
