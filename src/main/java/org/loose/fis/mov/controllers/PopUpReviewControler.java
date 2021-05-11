400815bf3010d26203d8aa8a701173d9154ebb4e
GramescuDan <gramescudan@yahoo.com>
2021-05-11 17:53:38 +0300

    MOV-14 Added PopUp for writing reviews

---
+++ src/main/java/org/loose/fis/mov/controllers/PopUpReviewControler.java
@@ -0,0 +1,18 @@
+package org.loose.fis.mov.controllers;
+
+import javafx.fxml.FXML;
+
+import java.awt.*;
+
+public class PopUpReviewControler {
+
+    @FXML
+    private TextField wreview;
+
+    public void saveButton(){
+
+    }
+    public void closeButton(){
+
+    }
+}
