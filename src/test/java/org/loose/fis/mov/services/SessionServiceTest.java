30c5d280d2fb944753e449f301307f6e4d2c969f
Ioan Hedeș <ihedes13@gmail.com>
2021-05-11 12:05:46 +0300

    MOV-117 Added tests for all the current methods in SessionServiceTest.

--- src/test/java/org/loose/fis/mov/services/SessionServiceTest.java
+++ src/test/java/org/loose/fis/mov/services/SessionServiceTest.java
@@ -1,15 +1,15 @@
 package org.loose.fis.mov.services;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.loose.fis.mov.exceptions.SessionAlreadyExistsException;
+import org.loose.fis.mov.model.Screening;
 import org.loose.fis.mov.model.User;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
-@Disabled
+import static org.junit.jupiter.api.Assertions.*;
+
 class SessionServiceTest {
 
     @BeforeEach
@@ -18,12 +18,81 @@ class SessionServiceTest {
 
     @AfterEach
     void tearDown() {
+        SessionService.destroySession();
     }
 
     @Test
-    void getLoggedInUser() {
+    @DisplayName("Test if sessions are created correctly")
+    void startSession() {
         User user = new User("test", "test", "test", "test", "test", "test");
-        assertDoesNotThrow(() -> SessionService.startSession(new User("test", "test", "test", "test", "test", "test")));
+        assertDoesNotThrow(() -> SessionService.startSession(new User(
+                "test",
+                "test",
+                "test",
+                "test",
+                "test",
+                "test"
+        )));
         assertEquals(SessionService.getLoggedInUser(), user);
+    }
+
+    @Test
+    @DisplayName("Test if two sessions cannot coexist")
+    void startSessionTwo() {
+        assertDoesNotThrow(() -> SessionService.startSession(new User(
+                "test",
+                "test",
+                "test",
+                "test",
+                "test",
+                "test"
+        )));
+        assertThrows(
+                SessionAlreadyExistsException.class,
+                () -> SessionService.startSession(new User(
+                        "test2",
+                        "test",
+                        "test",
+                        "test",
+                        "test",
+                        "test"
+                ))
+        );
+
+    }
+
+    @Test
+    @DisplayName("Test if sessions are erased correctly")
+    void destroySession() {
+        assertDoesNotThrow(() -> SessionService.startSession(
+                new User(
+                        "test",
+                        "test",
+                        "test",
+                        "test",
+                        "test",
+                        "test"
+                )));
+        SessionService.destroySession();
+        assertNull(SessionService.getLoggedInUser());
+    }
+
+    @Test
+    @DisplayName("Test if selected screenings are set correctly")
+    void setSelectedScreening() {
+        Screening screening = new Screening(null,
+                                            new GregorianCalendar(
+                                                    2021,
+                                                    Calendar.JANUARY,
+                                                    1,
+                                                    13,
+                                                    37
+                                            ).getTime(),
+                                            "test_movie",
+                                            "test_cinema",
+                                            10
+        );
+        SessionService.setSelectedScreening(screening);
+        assertEquals(screening, SessionService.getSelectedScreening());
     }
 }
